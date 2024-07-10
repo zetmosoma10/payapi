c78e41ee997bca93d779dc426ab28a346fac56e3
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-10 12:35:22 +0200

    add colors and typography styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // PRIMARY
+        dark_pink: "#ba4270",
+        water_white: "#fbfcfe",
+        san_juan_blue: "#36536b",
+        mirage_blue: "#1b262f",
+        charm_pink: "#da6d97",
+        light_san_juan_blue: "#6c8294",
+      },
+    },
   },
   plugins: [],
 };
