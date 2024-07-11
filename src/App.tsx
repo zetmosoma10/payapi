bd71eeba59fecd4c13cf20e69e57037ba5f23438
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-11 12:55:37 +0200

    build Stats component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Stats from "./pages/About/components/Stats";
 import Header from "./pages/About/sections/Header";
 import LogoCloud from "./pages/Home/sections/LogoCloud";
 import ProductFeatures from "./pages/Home/sections/ProductFeatures";
@@ -5,7 +6,7 @@ import ProductFeatures from "./pages/Home/sections/ProductFeatures";
 function App() {
   return (
     <div className="bg-background_color min-h-screen overflow-hidden">
-      <Header />
+      <Stats />
     </div>
   );
 }
---
+++ src/pages/About/components/Stats.tsx
@@ -0,0 +1,14 @@
+const Stats = () => {
+  return (
+    <div>
+      <p className="text-base text-san_juan_blue text-opacity-70">
+        Team Members
+      </p>
+      <h4 className="font-medium text-[56px] text-dark_pink tracking-[-0.43px] ">
+        300+
+      </h4>
+    </div>
+  );
+};
+
+export default Stats;
