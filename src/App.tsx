f9c5d1c9a159f6ca5b2f35797448ffe5fb824786
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-11 21:50:56 +0200

    build CallToAction section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import ActionPanel from "./components/ActionPanel";
-import Pricing from "./pages/Pricing/sections/Pricing";
+import CallToAction from "./components/CallToAction";
 
 function App() {
   return (
     <div className="bg-background_color min-h-screen overflow-hidden">
-      <ActionPanel />
+      <CallToAction />
     </div>
   );
 }
--- src/components/ActionPanel.tsx
+++ src/components/ActionPanel.tsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 const ActionPanel = () => {
   return (
-    <form className="flex flex-col gap-4 md:rounded-3xl md:flex-row md:items-center m-20 md:bg-water_white md:shadow-md">
+    <form className="flex flex-col gap-4 md:rounded-3xl md:flex-row md:items-center md:bg-water_white md:shadow-md">
       <input
         className="text-mirage_blue md:w-full bg-water_white pt-4 pb-[14px] pl-6 rounded-3xl placeholder:text-base placeholder:text-san_juan_blue
             placeholder:text-opacity-50 placeholder:font-bold shadow-md md:shadow-none outline-none"
