4797632a2a369eb310c62fb4c9c1ede9de116540
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-11 14:13:23 +0200

    build ImageShowcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import ImageShowcase from "./pages/About/sections/ImageShowcase";
 import PreviewStats from "./pages/About/sections/PreviewStats";
 
 function App() {
   return (
     <div className="bg-background_color min-h-screen overflow-hidden">
-      <PreviewStats />
+      <ImageShowcase />
     </div>
   );
 }
