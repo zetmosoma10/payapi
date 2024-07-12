3c387df55f8ead62ba41a2107f78efe9a7640b58
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-12 12:42:49 +0200

    build NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import CallToAction from "./components/CallToAction";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="bg-background_color min-h-screen overflow-hidden">
-      <Footer />
+      <NavBar />
     </div>
   );
 }
