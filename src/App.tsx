926182c05bf788727ddb986d8a7d99d024eeb25b
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-12 18:04:17 +0200

    build form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import SideBar from "./components/SideBar";
+import ContactForm from "./pages/Contacts/components/ContactForm";
 import Hero from "./pages/Home/sections/Hero";
 
 function App() {
   return (
     <div className="relative bg-background_color min-h-screen overflow-hidden">
-      <Hero />
+      <ContactForm />
     </div>
   );
 }
---
+++ src/pages/Contacts/components/ContactForm.tsx
@@ -0,0 +1,39 @@
+import Button from "../../../components/Button";
+
+const ContactForm = () => {
+  return (
+    <form className="space-y-6 m-20">
+      <input className="input" type="text" placeholder="Name" />
+      <div>
+        <input className="input" type="email" placeholder="Email Address" />
+        <small className="text-charm_pink ml-5 hidden">
+          This field can't be empty
+        </small>
+      </div>
+      <input className="input" type="text" placeholder="Company Name" />
+      <input className="input" type="text" placeholder="title" />
+      <textarea className="input" placeholder="Message"></textarea>
+      <div className="flex items-center  space-x-6">
+        <input
+          className="w-6 h-6 checked:bg-dark_pink"
+          type="checkbox"
+          id="checkbox"
+        />
+        <label
+          className="text-base text-san_juan_blue tracking-[-0.12px] "
+          htmlFor="checkbox"
+        >
+          Stay up-to-date with company announcements and updates to our API
+        </label>
+      </div>
+      <Button
+        classNames="btn-secondary px-[50px] text-san_juan_blue hover:border-0
+            border-san_juan_blue hover:bg-san_juan_blue hover:text-water_white"
+      >
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default ContactForm;
