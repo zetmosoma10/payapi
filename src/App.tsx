3927ed4430d3472ecdc2a6a4c009a5303b03f204
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-12 16:26:37 +0200

    build Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import SideBar from "./components/SideBar";
+import Hero from "./pages/Home/sections/Hero";
 
 function App() {
   return (
     <div className="relative bg-background_color min-h-screen overflow-hidden">
-      <SideBar />
+      <Hero />
     </div>
   );
 }
---
+++ src/pages/Home/sections/Hero.tsx
@@ -0,0 +1,32 @@
+import HeroImg from "../../../assets/home/desktop/illustration-phone-mockup.svg";
+import ActionPanel from "../../../components/ActionPanel";
+
+const Hero = () => {
+  return (
+    <section className="max-container">
+      <div className=" grid gap-x-16 min-[840px]:grid-cols-2 max-w-[520px] mx-auto min-[840px]:max-w-none min-[840px]:mx-0">
+        <img
+          className="w-full order-1 min-[840px]:order-2"
+          src={HeroImg}
+          alt=""
+        />
+        <div className="text-center max-w-[540px] order-2 min-[840px]:order-1 min-[840px]:text-left self-center">
+          <h1 className="font-medium text-san_juan_blue text-3xl tracking-[-0.25px] md:text-5xl lg:text-7xl  md:tracking-[-0.25px] lg:tracking-[-0.55px] ">
+            Start building with our APIs for absolutely free.
+          </h1>
+          <div className="mt-6">
+            <ActionPanel />
+            <p className="mt-6 text-light_san_juan_blue text-base">
+              Have any questions?{" "}
+              <a className="font-bold" href="">
+                Contact Us
+              </a>{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
