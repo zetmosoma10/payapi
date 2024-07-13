52c8c784ff76e43404d371f32bdd71c2ed4e3b87
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-13 16:04:21 +0200

    implement route animation

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,13 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Layout = () => {
+  const location = useLocation();
   return (
     <div className="relative bg-background_color min-h-screen overflow-hidden">
       <NavBar />
-      <Outlet />
+      <AnimatePresence>
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Outlet />
+        </motion.div>
+      </AnimatePresence>
       <div className="max-container  my-20 md:my-24">
         <CallToAction />
       </div>
