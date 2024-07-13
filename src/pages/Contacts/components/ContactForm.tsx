dfa0a23006a10bf2c1c016bc8805df9ac0eb133d
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-13 13:14:45 +0200

    Implement form with react-hook-form and integration with zod

--- src/pages/Contacts/components/ContactForm.tsx
+++ src/pages/Contacts/components/ContactForm.tsx
@@ -1,20 +1,105 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import Button from "../../../components/Button";
 
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must at least 3 characters." }),
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email address" }),
+  company: z.string().min(1, { message: "Company is required" }),
+  title: z.string().min(1, { message: "Title is required" }),
+  message: z
+    .string()
+    .max(200, { message: "Message must not be more than 200 characters." }),
+  checkbox: z.boolean(),
+});
+
+type FormData = z.infer<typeof schema>;
+
 const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <form className="space-y-6">
-      <input className="input" type="text" placeholder="Name" />
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
       <div>
-        <input className="input" type="email" placeholder="Email Address" />
-        <small className="text-charm_pink ml-5 hidden">
-          This field can't be empty
-        </small>
+        <input
+          {...register("name")}
+          className={`input ${errors.name && "border-b-dark_pink"}`}
+          type="text"
+          placeholder="Name"
+        />
+        {errors.name && (
+          <small className="text-charm_pink ml-5 ">{errors.name.message}</small>
+        )}
       </div>
-      <input className="input" type="text" placeholder="Company Name" />
-      <input className="input" type="text" placeholder="title" />
-      <textarea className="input" placeholder="Message"></textarea>
+      <div>
+        <input
+          {...register("email")}
+          className={`input ${errors.email && "border-b-dark_pink"}`}
+          type="email"
+          placeholder="Email Address"
+        />
+        {errors.email && (
+          <small className="text-charm_pink ml-5 ">
+            {errors.email.message}
+          </small>
+        )}
+      </div>
+      <div>
+        <input
+          {...register("company")}
+          className={`input ${errors.company && "border-b-dark_pink"}`}
+          type="text"
+          placeholder="Company Name"
+        />
+        {errors.company && (
+          <small className="text-charm_pink ml-5 ">
+            {errors.company.message}
+          </small>
+        )}
+      </div>
+      <div>
+        <input
+          {...register("title")}
+          className={`input ${errors.title && "border-b-dark_pink"}`}
+          type="text"
+          placeholder="title"
+        />
+        {errors.title && (
+          <small className="text-charm_pink ml-5 ">
+            {errors.title.message}
+          </small>
+        )}
+      </div>
+      <div>
+        <textarea
+          {...register("message")}
+          className={`input ${errors.message && "border-b-dark_pink"}`}
+          placeholder="Message"
+        ></textarea>
+        {errors.message && (
+          <small className="text-charm_pink ml-5 ">
+            {errors.message.message}
+          </small>
+        )}
+      </div>
       <div className="flex items-center  space-x-6">
         <input
+          {...register("checkbox")}
           className="w-6 h-6 checked:bg-dark_pink"
           type="checkbox"
           id="checkbox"
