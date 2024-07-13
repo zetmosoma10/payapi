2de961115339513a8fce1aa154fa98d80c780438
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-13 16:10:34 +0200

    remove animation

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes.tsx";
+import ScrollToTop from "./components/ScrollToTop.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}>
+      <ScrollToTop />
+    </RouterProvider>
   </React.StrictMode>
 );
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,25 +1,13 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import { AnimatePresence, motion } from "framer-motion";
 
 const Layout = () => {
-  const location = useLocation();
   return (
     <div className="relative bg-background_color min-h-screen overflow-hidden">
       <NavBar />
-      <AnimatePresence>
-        <motion.div
-          key={location.pathname}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <Outlet />
-        </motion.div>
-      </AnimatePresence>
+      <Outlet />
       <div className="max-container  my-20 md:my-24">
         <CallToAction />
       </div>
